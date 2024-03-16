9882aabf923584221415f2f6f2501e9813bfbf69
Eternity1001 <MMirhossain01@gmail.com>
2024-03-16 18:54:28 -0400

    Finished Login / sign up, Created a response for incorrect password or username

--- Backend/Login.py
+++ Backend/Login.py
@@ -1,6 +1,73 @@
-# class Login:
+import bcrypt
+import hashlib
+import uuid 
+from pymongo import MongoClient
+
+def loginAndRegisterDataBase():
+    client = MongoClient("mongo")
+    db = client["login"]
+    collection = db["login"]
+    return collection
+class LoginAndRegistration:
     
-#     def __init__(self) -> None:
-#         pass
+    def __init__(self, collection):
+        self.collection = collection    
+        self.errorMessage = ''
+    
+    def same_password(self, password, confPassword):
+        if password == confPassword:
+            return True
+        return False
+                
+    def existingAccount(self,username):
+        
+        for i in self.collection.find():
+            
+            if i['username'] == username:
+                
+                return [True, i]
+        
+        return [False, '']
+    
+    def strongPassword(self, password):
+        if len(password) >= 8:
+            return True
+        return False
+        
+    def create_account(self, username, password, confirmPassword):
+        
+        if not self.same_password(password, confirmPassword):
+            self.errorMessage = 'Password and Confirm Password dont match'
+            return False
+        if self.existingAccount(username)[0]:
+            self.errorMessage = 'Username already taken'
+            return False
+        if not self.strongPassword(password):
+            self.errorMessage = 'Password must be 8 Character minimum'
+            return False
+        hashedPassword = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        self.collection.insert_one({'username': username, 'password': hashedPassword, 'token':"N/A", 'xref':'N/A'})
+        return True
+    
+    def login(self, username, password):
+        
+        info = self.existingAccount(username)
+        
+        if info[0]: 
+            if bcrypt.checkpw(password.encode(), info[1]['password']):
+                id = str(uuid.uuid1())
+                hashedToken = hashlib.sha512(id.encode()).digest()
+                self.collection.update_one({'username': username}, {'$set': {'token': hashedToken}}, upsert=True)
+                
+                return [True, id]
+        
+        
+        self.errorMessage = 'Incorrect Username or Password'    
+            
+        return [False, 'Incorrect Username or Password']
+
+                
+            
+    
     
     
--- app.py
+++ app.py
@@ -1,21 +1,54 @@
-from flask import Flask, jsonify, render_template, url_for, request, redirect
+from flask import Flask, jsonify, render_template, url_for, request, redirect, Response, make_response
 from pymongo import MongoClient
 from Backend.database import loginAndRegisterDataBase
-app = Flask(__name__, static_url_path='/static') 
+from Backend.Login import LoginAndRegistration
 
+app = Flask(__name__, static_url_path='/static') 
+accountInfo = LoginAndRegistration(loginAndRegisterDataBase())
 @app.route("/", methods=["GET"])
 def loginPage():
-    return  render_template('index.html')
+    
+    response = make_response(render_template('/index.html', error=accountInfo.errorMessage))
+    accountInfo.errorMessage = ''
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Contection'] = 'keep-alive'
+    response.status_code = 200
+    return response
+
 
 @app.route('/login', methods=['POST'])
 def login():
     name = request.form['username']
-    password = request.form['password']
-        
-    return redirect('/homePage')
-@app.route('/homePage', methods=['GET', 'POST'])
-def homePage():
-    return render_template("homePage.html")
+    password = request.form['password'] 
+    info = accountInfo.login(name, password)
+    if info[0]:
+        response = make_response(render_template('/homePage.html'))
+        response.set_cookie('token', info[1], httponly=True, max_age=3600)
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['Contection'] = 'keep-alive'
+        return response
+    return redirect('/')
+
+@app.route("/Register", methods = ['GET', 'POST'])
+def registerPage():
+    
+    response = make_response(render_template('registerPage.html', error=accountInfo.errorMessage))
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Contection'] = 'keep-alive'
+    response.status_code = 200
+    accountInfo.errorMessage = ''
+    return response
+
+
+@app.route('/signup', methods=['POST'])
+def signUp():
+    name = request.form['newUsername']
+    password = request.form['newPassword']
+    confPassword = request.form['confirmPassword']
+    if accountInfo.create_account(name, password, confPassword):
+        return redirect('/')
+
+    return redirect('/Register')
 
 if __name__ == "__main__":
     # Please do not set debug=True in production
